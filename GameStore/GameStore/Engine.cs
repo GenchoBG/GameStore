970e71586bb9454ff4b478f54b1f3970d1fef85b
GenchoBG <genchobg11@gmail.com>
2019-05-28 22:06:59 +0300

    Create User & Role tables

--- GameStore/GameStore/Engine.cs
+++ GameStore/GameStore/Engine.cs
@@ -10,7 +10,7 @@ namespace GameStore
         private const string ConnectionString = "Server=localhost;Port=3306;Uid=root;Pwd=root;";
 
         private IDictionary<string, ICommand> commands;
-        
+
         public Engine()
         {
             this.Connection = new MySqlConnection(ConnectionString);
@@ -51,7 +51,33 @@ namespace GameStore
 
         private void InitialSetup()
         {
-            var query = @"";
+            var query = @"DROP DATABASE IF EXISTS GameStore;
+                            CREATE DATABASE GameStore CHARSET 'utf8';
+                            USE GameStore;
+
+                            CREATE TABLE Users(
+	                            Id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	                            Usermame VARCHAR(150) NOT NULL,
+	                            Password VARCHAR(150) NOT NULL,
+                                Balance DOUBLE NOT NULL
+                            );
+
+                            CREATE INDEX Index_User_Username ON Users (Usermame);
+
+                            CREATE TABLE Roles(
+	                            Id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	                            Name VARCHAR(150) NOT NULL
+                            );
+
+                            CREATE INDEX Index_Roles_Name ON Roles (Name);
+
+                            CREATE TABLE UserRoles(
+	                            UserId INTEGER NOT NULL,
+	                            RoleId INTEGER NOT NULL, 
+	                            FOREIGN KEY (UserId) REFERENCES Users(Id),
+	                            FOREIGN KEY (RoleId) REFERENCES Roles(Id),
+	                            PRIMARY KEY (UserId, RoleId)
+                            );";
 
             using (var cmd = new MySqlCommand(query, this.Connection))
             {
